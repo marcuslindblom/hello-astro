71524b187f7741a1d45c10e7ca7979bc1800daf0
Marcus Lindblom <m@marcuslindblom.com>
2025-06-02 09:49:51 +0200

    Add Vercel adapter to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { defineConfig } from 'astro/config';
 import strife from '@strifeapp/astro';
 import Homes from './src/collections/Homes';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [strife({
     collections: [Homes]
   })],
+
   output: 'server',
+  adapter: vercel(),
 });
